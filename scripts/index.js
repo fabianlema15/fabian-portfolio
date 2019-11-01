a14ed6fa1506f797ba68cb4d48b65c900c782d79
Fabian Lema <fabianlemac@gmail.com>
2019-11-01 15:59:09 -0500

    linkedlink updated

--- scripts/index.js
+++ scripts/index.js
@@ -16,7 +16,7 @@ const initial = (function(){
 
     function loadFooter(){
         $('footer').html(`<div class="footer-links">
-            <a href="https://www.linkedin.com/in/fabian-lema-canar-79539250" target="_blank"><img class="icon" src="images/linkedin.png"></a>
+            <a href="https://www.linkedin.com/in/fabian-lema-79539250" target="_blank"><img class="icon" src="images/linkedin.png"></a>
             <a href="mailto: fabianlemac@gmail.com"><img class="icon" src="images/email.png"></a>
             <a href="https://github.com/fabianlema15" target="_blank"><img class="icon" src="images/github.png"></a>
 						<a href="https://bitbucket.org/fabianlema" target="_blank"><img class="icon" src="images/bitbucket.png"></a>
@@ -95,7 +95,7 @@ const initial = (function(){
                 <div class="contact-links">
                     <span>if (</span>you want to view my profile<span>){</span>
 
-                    <p><a href="https://www.linkedin.com/in/fabian-lema-canar-79539250" target="_blank"><img class="icon" src="images/linkedin.png">LinkedIn Link</a></p>
+                    <p><a href="https://www.linkedin.com/in/fabian-lema-79539250" target="_blank"><img class="icon" src="images/linkedin.png">LinkedIn Link</a></p>
 
                     <span>} else if (</span>you want to check my code<span>) {</span>
 
